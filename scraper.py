77cf1a1a861977da721cd651dae50d1e7d5487b0
pcsanguinetti <pablo_sanguinetti@hotmail.com>
2021-05-04 10:35:14 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,17 @@
+# Importar dependencias
 from diario import *
 import pandas as pd
 
+# Abrir ficheros
 todos = pd.read_csv("data/todos.csv", index_col=[0])
 belicas = pd.read_csv("data/belicas.csv", index_col=[0])
 noesunaguerra = pd.read_csv("data/noesunaguerra.csv", index_col=[0])
 with open("data/pattern.txt", encoding="UTF-8") as f:
     pattern = f.read()
+with open("data/blacklist.txt") as b:
+    blacklist = b.read().splitlines() 
 
+# Crear objetos Diario y lista de objetos
 confi = Diario("El Confidencial", "https://www.elconfidencial.com/tags/temas/elecciones-madrid-21191", tag="article")
 abc = Diario("ABC", "https://www.abc.es/elecciones/elecciones-madrid/", class_="cuerpo-articulo")
 elpais = Diario("El País", "https://elpais.com/espana/elecciones-madrid/", complemento_url="https://elpais.com", tag="h2")
@@ -18,9 +23,11 @@ publico = Diario("Público", "https://www.publico.es/tag/elecciones-madrid/", co
 
 diarios = [confi, abc, elpais, elmundo, razon, diarioes, espanol, publico]
 
+# Cantidad actual de listas
 todos_antes = len(todos)
 belicas_antes = len(belicas)
 
+# Extracción de datos nuevos y unión con anteriores
 dfs = {"todos" : todos}
 dfs_belicas = {"belicas" : belicas}
 dfs_guerra = {"guerra" : noesunaguerra}
@@ -34,23 +41,23 @@ todos = pd.concat([df for df in dfs.values()]).reset_index(drop=True)
 belicas = pd.concat([df for df in dfs_belicas.values()]).reset_index(drop=True)
 noesunaguerra = pd.concat([df for df in dfs_guerra.values()]).reset_index(drop=True)
 
+# Información sobre nuevos datos y errores
 errores = len(todos[todos["Titular"]=="error"])
 print("Se agregaron {} entradas".format(len(todos)-todos_antes))
 print("Se agregaron {} palabras bélicas".format(len(belicas)-belicas_antes))
 print("Errores de parsing: ", errores)
 
+# Limpieza de tablas y ordenamiento por fecha
 todos.drop_duplicates(["Titular", "Diario"], inplace=True)
 todos = todos.drop(todos[todos["Titular"]=="error"].index)
 todos.sort_values(by="Fecha", inplace = True)
-
-with open("data/blacklist.txt") as b:
-    blacklist = b.read().splitlines() 
 
 belicas = belicas[~belicas["Titular"].isin(blacklist)]
 belicas.drop_duplicates(["Palabra", "Titular", "Diario"], inplace=True)
 belicas.sort_values(by="Fecha", inplace = True)
 noesunaguerra.drop_duplicates(["todo"], inplace=True)
 
+# Tabla nueva con resumen de datos
 df1 = belicas.groupby(by=["Diario"])["Palabra"].count().reset_index()
 df2 = todos.groupby(by=["Diario"]).agg({
     "Titular" : "count",
@@ -59,10 +66,10 @@ df2 = todos.groupby(by=["Diario"]).agg({
     "Neu" : "mean"}).reset_index()
 
 resumen = pd.merge(df1, df2, on="Diario")
-
 resumen["Porcentaje"] = resumen["Palabra"] / resumen["Titular"] * 100
 resumen.sort_values("Porcentaje", ascending=False, inplace=True)
 
+# Guardar tablas de datos
 todos.to_csv("data/todos.csv")
 belicas.to_csv("data/belicas.csv")
 resumen.to_csv("data/resumen.csv")
